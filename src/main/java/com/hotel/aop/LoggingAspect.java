595c4e1461592ecbf4f5976c9541693597853551
Anton office <antonkol1993@gmail.com>
2025-07-15 17:25:43 +0300

    настроил через docker-compose Elasticserach, KIBANA, filebeat
    пытаюсь запустить

---
+++ src/main/java/com/hotel/aop/LoggingAspect.java
@@ -0,0 +1,25 @@
+package com.hotel.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Around("execution(* com.hotel..*(..))")
+    public Object logExecution(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long elapsed = System.currentTimeMillis() - start;
+        log.info("Method: {} took {} ms", joinPoint.getSignature(), elapsed);
+
+        return result;
+    }
+}
